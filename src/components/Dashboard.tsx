dd210abc57386c68cf628f4d39caf90c469c6131
deepak104080 <deepak104080@gmail.com>
2022-08-19 12:36:27 +0530

    19th August 2022 - Session

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 const Dashboard = () => {
+
+  const [examList, setExamList] = useState<any []>([]);
+
+  const fetchExams = async () => {
+    const url = 'http://localhost:4000/exams/fetchallexams';
+    const response = await axios.get(url);
+    console.log(response);
+    setExamList(response.data);
+  }
+
+  useEffect(() => {
+    fetchExams();
+  }, [])
+
+  const startExam = () => {
+    //api call to start exam
+    //fetch list of qs and store locally
+    //create entry in examlog table and fetch examlogid
+    //store examlog id locally
+  }
+
+
   return (
     <div className='row'>
         <div className='col-3 bg-warning bg-opacity-50'>
@@ -11,6 +34,40 @@ const Dashboard = () => {
             <Link to="" className='btn btn-success'>View Results</Link>
         </div>
         <div className='col-9 bg-warning bg-opacity-10'>
+            <table>
+              <thead>
+                <tr>
+                  <th>Sr No</th>
+                  <th>exam_name</th>
+                  <th>subject_id</th>
+                  <th>date</th>
+                  <th>time</th>
+                  <th>duration</th>
+                  <th>total_questions</th>
+                  <th>negativemarking</th>
+                  <th></th>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  examList && examList.map((item, index) => (
+                    <tr>
+                      <td>{item.exam_id}</td>
+                      <td>{item.exam_name}</td>
+                      <td>{item.subject_id}</td>
+                      <td>{item.date}</td>
+                      <td>{item.time}</td>
+                      <td>{item.duration}</td>
+                      <td>{item.total_questions}</td>
+                      <td>{item.negativemarking}</td>
+                      <td>
+                        <button type='button' onClick={startExam} className="btn btn-primary btn-sm">Start Exam</button>
+                      </td>
+                    </tr>
+                  ))
+                }
+              </tbody>
+            </table>
             
         </div>
     </div>
